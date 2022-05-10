1688eca7b6b65cdfd66a54bd06917e7680c193eb
Raindrop <collins.odinga@student.moringaschool.com>
2022-05-11 00:32:21 +0300

    feat: creating tests

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,4 +1,17 @@
+import models.Animal;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AnimalTest {
+
+
+
+    @Test
+    public void animal_instantiatesCorrectly_true() {
+        Animal animal = new Animal("Lion", "carnivore","adult","healthy", "netiar", "14.00HRS 6/7/2019" );
+        assertEquals(true, animal instanceof Animal);
+    }
 
 
 
